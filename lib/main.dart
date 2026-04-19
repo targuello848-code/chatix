6dfe503c7b8c2523c96aa9982b56cdba4291f484
targuello848-code <targuello848@gmail.com>
2026-04-18 23:59:12 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("ChatiX funcionando"),
+        ),
+      ),
+    );
+  }
+}
